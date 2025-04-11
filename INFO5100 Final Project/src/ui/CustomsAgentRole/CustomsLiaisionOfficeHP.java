37b27c1253a730d2d22b8e86dd39f0918d54fbdc
ChenyanFR <zhu.chenya@northeastern.edu>
2025-04-11 19:02:10 -0400

    updated liaision details

--- INFO5100 Final Project/src/ui/CustomsAgentRole/CustomsLiaisionOfficeHP.java
+++ INFO5100 Final Project/src/ui/CustomsAgentRole/CustomsLiaisionOfficeHP.java
@@ -30,11 +30,11 @@ public class CustomsLiaisionOfficeHP extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 837, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 661, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
