510b025f40c2965f6d8a2ec10387d91ace869a52
YushengShi <shi.yush@northeastern.edu>
2025-04-18 20:24:35 -0400

    button imp.

--- INFO5100 Final Project/src/ui/AdminRole/ManageOwnProfile.java
+++ INFO5100 Final Project/src/ui/AdminRole/ManageOwnProfile.java
@@ -4,12 +4,17 @@
  */
 package ui.AdminRole;
 
+import javax.swing.JPanel;
+
 /**
  *
  * @author yushe
  */
 public class ManageOwnProfile extends javax.swing.JPanel {
 
+    JPanel workArea;
+    
+    
     /**
      * Creates new form ManageOwnProfile
      */
@@ -35,6 +40,11 @@ public class ManageOwnProfile extends javax.swing.JPanel {
         btnSave = new javax.swing.JButton();
 
         btnBack.setText("<< Back");
+        btnBack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBackActionPerformed(evt);
+            }
+        });
 
         lblUN.setText("Username:");
 
@@ -93,6 +103,11 @@ public class ManageOwnProfile extends javax.swing.JPanel {
                 .addContainerGap(87, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_btnBackActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
